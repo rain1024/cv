26704af298f6f295d32af871e6120ba2430be4ee
Vu Anh <anhv.ict91@gmail.com>
2020-11-20 12:07:03 +0700

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-name="cv"
+name="anhv_cv"
 pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=out $name.tex
 bibtex $name.aux
 pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=out $name.tex
